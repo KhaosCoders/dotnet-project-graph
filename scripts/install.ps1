786320f3db7b62009acc1e9d143d5e2f655ce8ec
Khaos <khaos@khaos-coders.org>
2023-03-02 10:22:40 +0100

    Fixed typo in packageid

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,9 +1,9 @@
 function Install-Tool {
-    dotnet tool install -g --add-source ./nupkg DotNet.ProjectGraph.Tool
+    dotnet tool install -g --add-source ./nupkg Project.Graph.Tool
 }
 
 if (-Not (Install-Tool)) {
     Write-Host "Installing tool failed, trying to uninstall first..."
-    dotnet tool uninstall -g DotNet.ProjectGraph.Tool
+    dotnet tool uninstall -g Project.Graph.Tool
     Install-Tool
 }
